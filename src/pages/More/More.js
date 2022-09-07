424244a7c056335ebcc620bd8921c8e1c0925a8d
denghaoming <75519a8a517726839c6df22b8669233c1370b881>
2022-09-07 21:07:44 +0800

    1

--- src/pages/More/More.js
+++ src/pages/More/More.js
@@ -98,7 +98,7 @@ class More extends Component {
 
                 <div className="button ModuleTop" onClick={this.routerTo.bind(this,'/collect')}>归集钱包</div>
 
-                <div className="button ModuleTop" onClick={this.routerTo.bind(this,'/checkCreateContract')}>加撤池子不需要手续费的钱包地址</div>
+                <div className="button ModuleTop" onClick={this.routerTo.bind(this,'/checkCreateContract')}>创建能够部署token1的钱包</div>
                 
                 <div className='QQ'>
                     <div className='Text'>联系我们</div>
